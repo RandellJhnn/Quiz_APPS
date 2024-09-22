b7f52b15d28ee71a65497c380a3fa565259b31bf
Administrator <Administrator@RANDELLJOHN>
2024-09-22 19:55:11 +0800

    Add project files.

---
+++ Quiz_APPS/QUIZ_APP.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Quiz_APPS
+{
+    partial class QUIZ_APP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1_Questions = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButtonOption1 = new System.Windows.Forms.RadioButton();
+            this.button_Next = new System.Windows.Forms.Button();
+            this.radioButtonOption2 = new System.Windows.Forms.RadioButton();
+            this.radioButtonOption3 = new System.Windows.Forms.RadioButton();
+            this.radioButtonOption4 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // richTextBox1_Questions
+            // 
+            this.richTextBox1_Questions.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1_Questions.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1_Questions.Name = "richTextBox1_Questions";
+            this.richTextBox1_Questions.Size = new System.Drawing.Size(575, 123);
+            this.richTextBox1_Questions.TabIndex = 0;
+            this.richTextBox1_Questions.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Location = new System.Drawing.Point(12, 141);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(575, 4);
+            this.panel1.TabIndex = 1;
+            // 
+            // radioButtonOption1
+            // 
+            this.radioButtonOption1.AutoSize = true;
+            this.radioButtonOption1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonOption1.Location = new System.Drawing.Point(12, 188);
+            this.radioButtonOption1.Name = "radioButtonOption1";
+            this.radioButtonOption1.Size = new System.Drawing.Size(152, 29);
+            this.radioButtonOption1.TabIndex = 2;
+            this.radioButtonOption1.TabStop = true;
+            this.radioButtonOption1.Text = "radioButton1";
+            this.radioButtonOption1.UseVisualStyleBackColor = true;
+            this.radioButtonOption1.Click += new System.EventHandler(this.radioButtonOption1_Click);
+            // 
+            // button_Next
+            // 
+            this.button_Next.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button_Next.FlatAppearance.BorderSize = 0;
+            this.button_Next.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Next.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button_Next.Location = new System.Drawing.Point(79, 372);
+            this.button_Next.Name = "button_Next";
+            this.button_Next.Size = new System.Drawing.Size(416, 47);
+            this.button_Next.TabIndex = 3;
+            this.button_Next.Text = "Next";
+            this.button_Next.UseVisualStyleBackColor = false;
+            this.button_Next.Click += new System.EventHandler(this.button_Next_Click);
+            // 
+            // radioButtonOption2
+            // 
+            this.radioButtonOption2.AutoSize = true;
+            this.radioButtonOption2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonOption2.Location = new System.Drawing.Point(12, 232);
+            this.radioButtonOption2.Name = "radioButtonOption2";
+            this.radioButtonOption2.Size = new System.Drawing.Size(152, 29);
+            this.radioButtonOption2.TabIndex = 4;
+            this.radioButtonOption2.TabStop = true;
+            this.radioButtonOption2.Text = "radioButton2";
+            this.radioButtonOption2.UseVisualStyleBackColor = true;
+            this.radioButtonOption2.Click += new System.EventHandler(this.radioButtonOption2_Click);
+            // 
+            // radioButtonOption3
+            // 
+            this.radioButtonOption3.AutoSize = true;
+            this.radioButtonOption3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonOption3.Location = new System.Drawing.Point(12, 281);
+            this.radioButtonOption3.Name = "radioButtonOption3";
+            this.radioButtonOption3.Size = new System.Drawing.Size(152, 29);
+            this.radioButtonOption3.TabIndex = 5;
+            this.radioButtonOption3.TabStop = true;
+            this.radioButtonOption3.Text = "radioButton3";
+            this.radioButtonOption3.UseVisualStyleBackColor = true;
+            this.radioButtonOption3.Click += new System.EventHandler(this.radioButtonOption3_Click);
+            // 
+            // radioButtonOption4
+            // 
+            this.radioButtonOption4.AutoSize = true;
+            this.radioButtonOption4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonOption4.Location = new System.Drawing.Point(12, 326);
+            this.radioButtonOption4.Name = "radioButtonOption4";
+            this.radioButtonOption4.Size = new System.Drawing.Size(152, 29);
+            this.radioButtonOption4.TabIndex = 6;
+            this.radioButtonOption4.TabStop = true;
+            this.radioButtonOption4.Text = "radioButton4";
+            this.radioButtonOption4.UseVisualStyleBackColor = true;
+            this.radioButtonOption4.Click += new System.EventHandler(this.radioButtonOption4_Click);
+            // 
+            // QUIZ_APP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(600, 457);
+            this.Controls.Add(this.radioButtonOption4);
+            this.Controls.Add(this.radioButtonOption3);
+            this.Controls.Add(this.radioButtonOption2);
+            this.Controls.Add(this.button_Next);
+            this.Controls.Add(this.radioButtonOption1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richTextBox1_Questions);
+            this.Name = "QUIZ_APP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QUIZ_APP";
+            this.Load += new System.EventHandler(this.QUIZ_APP_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1_Questions;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButtonOption1;
+        private System.Windows.Forms.Button button_Next;
+        private System.Windows.Forms.RadioButton radioButtonOption2;
+        private System.Windows.Forms.RadioButton radioButtonOption3;
+        private System.Windows.Forms.RadioButton radioButtonOption4;
+    }
+}
---
+++ Quiz_APPS/Quiz Apps/QUIZ_APP.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_APPS
+{
+    public partial class QUIZ_APP : Form
+    {
+        public QUIZ_APP()
+        {
+            InitializeComponent();
+        }
+
+        // create an array of questiongs
+        String[] questions = { "What is the correct way to declare a variable in Visual Basic?", "Which event occurs when a button is clicked in a Visual Basic application??",
+                               "How do you create a message box in Visual Basic?", "What keyword is used to define a subroutine in Visual Basic?",
+                                "Which of the following statements is used to handle exceptions in Visual Basic?", };
+
+        // create an array of options 
+        // 5 questions
+        // 5 options
+        // the last option will be the correct answer
+        // and we will use it to check if the user select the correct answer
+        String[,] options = new string[5, 5] {{"Dim x As Integer", "Variable x Integer", "Int x", "Declare x As Integer", "Dim x As Integer"},
+                                               {"MouseHover", "KeyPress", "Click", "Load", "Click"},
+                                                {"MessageBox.Show(\"Hello World!\")", "ShowMessage(\"Hello World!\")", "Alert(\"Hello World!\")", "MsgBox.Display(\"Hello World!\")", "MessageBox.Show(\"Hello World!\")"},
+                                                 {"Function", "Sub", "Method", "Procedure", "Sub"},
+                                                  {"Try...Except", "Catch...Finally", "Error", "Handle", "Try...Except"}};
+
+        int index = 0, correct = 0;
+        private IEnumerable<Control> controls;
+
+        // create a function to check if the user has selected the correct answer
+        public void checkAnswer(RadioButton rbt)
+        {
+            // the 4th item(we start from 0) is the correct in every question
+            if (rbt.Text.Equals(options[index, 4]))
+            {
+                correct++;
+                // you can make the correct answer become green
+                // and the wrong answer red
+                rbt.BackColor = Color.Green;
+            }
+            else
+            {
+                rbt.BackColor = Color.Red;
+            }
+
+            index++;
+            // display radiobuttons
+            setEnable(false);
+        }
+
+        // create a function to uncheck radiobuttons
+        public void uncheck()
+        {
+            radioButtonOption1.Checked = false;
+            radioButtonOption2.Checked = false;
+            radioButtonOption3.Checked = false;
+            radioButtonOption4.Checked = false;
+        }
+
+        // create a function to change radiobuttons backcolor and color
+        public void resetRadio()
+        {
+            // we are gonna use the foreach loop
+            // the color change automatically when disabled
+            foreach(Control c in this.Controls)
+            {
+                if(c is RadioButton)
+                {
+                    RadioButton rdb = (RadioButton)c;
+                    rdb.BackColor = Color.White;
+                }
+            }
+
+        }
+
+        // create a function to enable/disable radiobuttons
+        public void setEnable(Boolean yes_or_no)
+        {
+            radioButtonOption1.Enabled = yes_or_no;
+            radioButtonOption2.Enabled = yes_or_no;
+            radioButtonOption3.Enabled = yes_or_no;
+            radioButtonOption4.Enabled = yes_or_no;
+        }
+
+        private void QUIZ_APP_load(object sender, EventArgs e)
+        {
+            // call the button click even
+            button_Next.PerformClick();
+        }
+
+        private void button_Next_Click(object sender, EventArgs e)
+        {
+            resetRadio();
+
+            if (button_Next.Text.Equals("Restart The Quiz"))
+            {
+                index = 0;
+                correct = 0;
+                richTextBox1_Questions.BackColor = Color.White;
+                richTextBox1_Questions.ForeColor = Color.Black;
+                button_Next.Text = "Next";
+            }   
+
+            if (index == questions.Length)
+            {
+                richTextBox1_Questions.Text = "Your Score:" + correct + " / " + questions.Length;
+
+                if (correct >= (float)questions.Length / 2)
+                {
+                    richTextBox1_Questions.BackColor = Color.Green;
+                    richTextBox1_Questions.ForeColor = Color.White;
+                }
+                else
+                {
+                    richTextBox1_Questions.BackColor = Color.Red;
+                    richTextBox1_Questions.ForeColor = Color.White;
+                }
+
+                button_Next.Text = "Restart The Quiz";
+
+            }
+            else
+            {
+                // enable radiobuttons
+                setEnable(true);
+                //uncheck radiobuttons
+                uncheck();
+                // display the question
+                richTextBox1_Questions.Text = questions[index];
+                // display  options
+                radioButtonOption1.Text = options[index, 0];
+                radioButtonOption2.Text = options[index, 1];
+                radioButtonOption3.Text = options[index, 2];
+                radioButtonOption4.Text = options[index, 3];
+
+                if(index == questions.Length - 1)
+                {
+                    button_Next.Text = "Finish and See Your Result";
+                }
+            }   
+        }
+
+        private void radioButtonOption1_Click(object sender, EventArgs e)
+        {
+            checkAnswer(radioButtonOption1);
+
+        }
+
+        private void radioButtonOption2_Click(object sender, EventArgs e)
+        {
+            checkAnswer(radioButtonOption2);
+
+        }
+
+        private void radioButtonOption3_Click(object sender, EventArgs e)
+        {
+            checkAnswer(radioButtonOption3);
+
+        }
+
+        private void radioButtonOption4_Click(object sender, EventArgs e)
+        {
+            checkAnswer(radioButtonOption4);
+
+        }
+
+        private void QUIZ_APP_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
